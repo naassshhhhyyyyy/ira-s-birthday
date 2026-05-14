e62da3123e37d36a456eff2a4e89d1ad26f93af7
naassshhhhyyyyy <nashabuel24@gmail.com>
2026-05-14 15:44:39 +0800

    Restore targetDate to specific future date

--- js/birthday.js
+++ js/birthday.js
@@ -9,8 +9,7 @@ let cakeClicked = false;
 // 🔒 SECURITY
 let isUnlocked = false;
 let lastTimeCheck = Date.now();
-//const targetDate = new Date("May 19, 2026 00:00:00").getTime();
-const targetDate = new Date().getTime() + 5000;
+const targetDate = new Date("May 19, 2026 00:00:00").getTime();
 
 // DOM elements
 const countEl = document.getElementById('count');
