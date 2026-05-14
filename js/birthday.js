47583c6c71b2425285998aaf34428f75d6357a36
naassshhhhyyyyy <nashabuel24@gmail.com>
2026-05-14 15:38:05 +0800

    Comment out targetDate for future date calculation

--- js/birthday.js
+++ js/birthday.js
@@ -9,7 +9,7 @@ let cakeClicked = false;
 // 🔒 SECURITY
 let isUnlocked = false;
 let lastTimeCheck = Date.now();
-// const targetDate = new Date("May 19, 2026 00:00:00").getTime();
+//const targetDate = new Date("May 19, 2026 00:00:00").getTime();
 const targetDate = new Date().getTime() + 5000;
 
 // DOM elements
